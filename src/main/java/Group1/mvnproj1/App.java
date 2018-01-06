50de726599446f27f33e233429cccbdd8770c5ff
hrishikeshh <hrishikeshh@hrishikeshh-PC>
2018-01-06 15:12:32 +0530

    first push

---
+++ src/main/java/Group1/mvnproj1/App.java
@@ -0,0 +1,19 @@
+package Group1.mvnproj1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriver driver=new FirefoxDriver();
+        driver.get("https://google.com");
+        
+        // ihlkgikuv
+    }
+}
